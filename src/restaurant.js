ad9c5e522b403fc621a3a63f5a938a7fe76d65c7
afzaman <afzaman2003@gmail.com>
2020-07-08 06:15:54 -0400

    step 5 completed

---
+++ src/restaurant.js
@@ -0,0 +1,7 @@
+var container = document.getElementById("content");
+var title = document.createElement("div");
+title.innerHTML("This Restaurant");
+title.appendChild(container);
+var img = document.createElement("img");
+img.source = "https://bakingamoment.com/wp-content/uploads/2020/03/IMG_7844-chocolate-babka.jpg";
+img.appendChild(container);
